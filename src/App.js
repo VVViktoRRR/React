fd02493a5ab88264cab7bb3a4808bcab3cacb7dd
VVViktoRRR <viktorusenko1604@gmail.com>
2022-01-23 10:07:19 +0200

    Lesson 2 CW

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import Form from "./components/form/Form";
+import Users from "./components/users/Users";
+import {useEffect, useState} from "react";
+import {userService} from "./services/user.service";
 
 function App() {
+   const [users, setUsers] = useState( []);
+    const [filteredusers, setFilteredusers] = useState( []);
+
+   useEffect(()=> {
+        userService.getAll().then(value => {
+            setUsers([...value])
+            setFilteredusers([...value])
+        })
+   }, [])
+
+  const getFilter =(data) => {
+    let filteredArr = [...users]
+      if (data.name) {
+          filteredArr = filteredArr.filter(user => user.name.toLowerCase().includes(data.name.toLowerCase()))
+      }
+      if (data.username) {
+          filteredArr = filteredArr.filter(user => user.username.toLowerCase().includes(data.username.toLowerCase()))
+      }
+      if (data.email) {
+          filteredArr = filteredArr.filter(user => user.email.toLowerCase().includes(data.email.toLowerCase()))
+      }
+      setFilteredusers(filteredArr)
+  }
   return (
     <div>
-<h1> hi people</h1>
+        <Form getFilter={getFilter}/>
+        <Users users ={filteredusers}/>
     </div>
   );
 }
---
+++ src/components/user/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = ({user}) => {
+    return (
+        <div>
+            {user.id})  {user.name} -- {user.username} -- {user.email}
+        </div>
+    );
+};
+
+export default User;
